248267f9c2cd327f8385284335b193d54c641ea5
Jack Skrable <jack.skrable@bybeam.co>
2025-03-18 08:17:57 -0400

    photo gallery started
    Signed-off-by: Jack Skrable <jack.skrable@bybeam.co>

--- src/app/photos/page.tsx
+++ src/app/photos/page.tsx
@@ -1,7 +1,7 @@
 import fs from "node:fs";
 import path from "node:path";
-import * as AspectRatio from "@radix-ui/react-aspect-ratio";
 import Image from "next/image";
+import Link from "next/link";
 import resolveAssetPath from "../utils/resolveAssetPath";
 import styles from "./page.module.css";
 
@@ -12,17 +12,33 @@ export default function PhotoBook() {
 	const photos = filenames.map((name) => path.join("/", "photos", name));
 
 	return (
-		<div className={styles.photoContainer}>
-			{photos.map((path) => (
-				<AspectRatio.Root key={path} ratio={16 / 9}>
-					<Image
-						src={path}
-						alt={path.split("/")[2].split(".")[0]}
-						style={{ objectFit: "contain" }}
-						fill
-					/>
-				</AspectRatio.Root>
-			))}
+		<div className={styles.container}>
+			<div className={styles.gallery}>
+				{photos.map((path) => (
+					<Link
+						key={path}
+						href={`/?path=${path}`}
+						as={`/p${path}`}
+						// ref={path === Number(lastViewedPhoto) ? lastViewedPhotoRef : null}
+						className={styles.thumbnailContainer}
+						shallow
+					>
+						<Image
+							alt={path.split("/")[2].split(".")[0]}
+							className={styles.thumbnail}
+							placeholder="blur"
+							blurDataURL={path}
+							src={path}
+							width={720}
+							height={480}
+							sizes="(max-width: 640px) 100vw,
+                  (max-width: 1280px) 50vw,
+                  (max-width: 1536px) 33vw,
+                  25vw"
+						/>
+					</Link>
+				))}
+			</div>
 		</div>
 	);
 }
